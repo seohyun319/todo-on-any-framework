3ca78dd3675c9a228fa4bbaadc688bc3a20aed20
seohyun319 <tjguselly0319@gmail.com>
2023-01-10 21:07:23 +0900

    [feat] ng g c todo-background

---
+++ angular-todo/src/app/todo-background/todo-background.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+// ng g c todo-background
+
+@Component({
+  selector: 'app-todo-background',
+  templateUrl: './todo-background.component.html',
+  styleUrls: ['./todo-background.component.css'],
+})
+export class TodoBackgroundComponent {}
