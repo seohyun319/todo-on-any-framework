d47775dca3dc12485d76294df3ef0f3a5bfb6083
seohyun319 <tjguselly0319@gmail.com>
2023-01-11 20:44:44 +0900

    [feat] ng g c todo-list, set todo list logic

--- angular-todo/src/app/app.module.ts
+++ angular-todo/src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoBackgroundComponent } from './todo-background/todo-background.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
-  declarations: [AppComponent, TodoBackgroundComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, TodoBackgroundComponent, TodoListComponent],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    //  AppRoutingModule,
+    RouterModule.forRoot([{ path: '', component: TodoListComponent }]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
