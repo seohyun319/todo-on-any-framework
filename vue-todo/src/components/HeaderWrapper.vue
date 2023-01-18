f1702b4166a5a43471108d49f8dd934d91cd553f
seohyun319 <tjguselly0319@gmail.com>
2023-01-18 16:40:09 +0900

    [feat] set HeaderWrapper, MainWrapper

---
+++ vue-todo/src/components/HeaderWrapper.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>Todo App with Vue</header>
+</template>
+
+<!-- scoped를 통해 해당 스코프 안의 헤더에만 스타일링 적용됨 -->
+<style scoped>
+header {
+  position: relative;
+  font-weight: 800;
+  font-size: 50px;
+  padding-top: 2em;
+  background: -webkit-linear-gradient(315deg, #42d392 25%, #647eff);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+</style>
---
+++ vue-todo/src/components/MainWrapper.vue
@@ -0,0 +1,49 @@
+<template>
+  <main>
+    <TodoInput @addTodoItem="addTodoItem" />
+    <!-- props 받아서 전달해주는 역할 -->
+    <TodoList
+      :todoList="todoList"
+      @removeTodoItem="removeTodoItem"
+      @toggleTodoItem="toggleTodoItem"
+    />
+    <TodoReset @resetTodoList="resetTodoList" />
+  </main>
+</template>
+
+<script lang="ts">
+import TodoInput from "./TodoInput.vue";
+import TodoList from "./TodoList.vue";
+import TodoReset from "./TodoReset.vue";
+
+export default {
+  components: {
+    TodoInput: TodoInput,
+    TodoList: TodoList,
+    TodoReset: TodoReset,
+  },
+  props: ["todoList"],
+  methods: {
+    addTodoItem(todo: string) {
+      // this.$emit로 addTodoItem 이벤트 가져오고 todo라는 변수 넣어줌
+      this.$emit("addTodoItem", todo);
+    },
+    toggleTodoItem(targetId: number) {
+      this.$emit("toggleTodoItem", targetId);
+    },
+    removeTodoItem(targetId: number) {
+      this.$emit("removeTodoItem", targetId);
+    },
+    resetTodoList() {
+      this.$emit("resetTodoList");
+    },
+  },
+};
+</script>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+}
+</style>
