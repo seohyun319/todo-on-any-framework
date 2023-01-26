6be45b320a34a34af75aeb46b4f13429cda09487
seohyun319 <tjguselly0319@gmail.com>
2023-01-27 08:01:53 +0900

    [feat] set store: writable todoData

--- svelte-todo/src/components/TodoList.svelte
+++ svelte-todo/src/components/TodoList.svelte
@@ -1,25 +1,8 @@
 <script>
+	import { todoData } from '../store';
 	import TodoItem from './TodoItem.svelte';
-
-	const todoList = [
-		{
-			id: 1,
-			text: '작살나게 놀기',
-			completed: true
-		},
-		{
-			id: 2,
-			text: '신명나는 스벨트 공부',
-			completed: false
-		},
-		{
-			id: 3,
-			text: '갓생 살기',
-			completed: false
-		}
-	];
 </script>
 
-{#each todoList as todo (todo.id)}
+{#each $todoData as todo (todo.id)}
 	<TodoItem {todo} />
 {/each}
