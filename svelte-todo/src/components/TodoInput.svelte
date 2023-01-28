5406aff5b316d046e33ecb90ffdf600534048229
seohyun319 <tjguselly0319@gmail.com>
2023-01-28 10:39:22 +0900

    [feat] set handleAddTodo

--- svelte-todo/src/components/TodoInput.svelte
+++ svelte-todo/src/components/TodoInput.svelte
@@ -1,6 +1,21 @@
 <script>
-	const handleSubmit = () => {};
+	import { todoData } from '../store';
+
+	let todoText = '';
+
+	const handleAddTodo = () => {
+		if (!todoText) return;
+
+		const newTodo = {
+			id: Date.now(),
+			text: todoText,
+			completed: false
+		};
+
+		$todoData = [...$todoData, newTodo];
+		todoText = '';
+	};
 </script>
 
-<input type="text" placeholder="오늘 뭐 할 거야?" />
-<button on:click={handleSubmit}>입력</button>
+<input type="text" placeholder="오늘 뭐 할 거야?" bind:value={todoText} />
+<button on:click={handleAddTodo}>입력</button>
