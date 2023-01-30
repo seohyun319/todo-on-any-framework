d663aee90b1bd13b2ecb2d3ded5cc069bb19bb3b
seohyun319 <tjguselly0319@gmail.com>
2023-01-31 08:29:17 +0900

    [feat] set total

---
+++ svelte-todo/src/components/Total.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { todoData } from '../store';
+
+	let completedTodo = 0;
+	$: completedTodo = $todoData.filter((todo) => todo.completed).length;
+	$: ratio = $todoData.length > 0 ? (completedTodo / $todoData.length).toFixed(2) : 0;
+</script>
+
+<div>Total: {+ratio * 100} %</div>
